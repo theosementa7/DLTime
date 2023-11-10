53742676ea773e2ed74959ebaa13b081d48022f4
Theo Sementa <kaayzenn7@gmail.com>
2023-11-10 13:29:44 +0100

    Initial Commit

---
+++ DLTime/DLTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLTimeApp.swift
+//  DLTime
+//
+//  Created by KaayZenn on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DLTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
